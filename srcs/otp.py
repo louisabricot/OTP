fdcf00798ad6643a6e88297e26f88dcea4463e96
louisabricot <louisabricot@protonmail.com>
2023-10-06 17:19:03 +0200

    cleaning

--- srcs/otp.py
+++ srcs/otp.py
@@ -3,83 +3,115 @@ import logging
 import sys
 import os
 import stat
-from cryptography.fernet import Fernet
+from cryptography.fernet import Fernet, InvalidToken
 import hashlib
 from datetime import datetime, timezone
 import hmac
 from math import floor
 import base64
+
 master_key_file = "ft_otp.key"
 
+def generate_master_key(verbose: bool, master_key: str) -> None:
+    """
+    """
 
-#t0 = 0
-#X = interval_time (30s)
-#t = datetime.datetime.now() - t0 / X
-
-# totp = hotp(master_key, t)
-
-def generate_master_key(master_key: str):
     try:
         with open(master_key_file, "w") as f:
-            os.chmod(master_key_file, stat.S_IRWXU)
-            print("original text:", master_key)
+            os.chmod(master_key_file, stat.S_IRUSR | stat.S_IWUSR)
+            
+            # Generate cypher key 
             key = Fernet.generate_key()
+            print("Fernet key: ", key.decode("utf-8"))
+
             fernet = Fernet(key)
             encrypted_key = fernet.encrypt(master_key.encode())
-            print("Fernet key: ", key.decode("utf-8"))
+            print(encrypted_key)
+
+            # Store encrypted key into file
             f.write(encrypted_key.decode("utf-8"))
+
             print("Successfully stored new master key!")
     except IOError as error:
-        print(error)
-    print("Generate master key")
+        logging.error(error)
+
+def decrypt(encrypted: str) -> str:
+    """
+    """
+    try:
+        password = input("Enter password:")
+        fernet = Fernet(bytes(password, 'utf-8'))
+        # TODO: tester avec un encrypted_file vide
+        # TODO: checker les valeurs de retour de Fernet
+        decrypted = fernet.decrypt(bytes(encrypted, 'utf-8')).decode()
+        return decrypted
+    except InvalidToken as e:
+        logging.error(f"Fernet raised an Invalid Token error: could not decrypt the master key")
+        sys.exit(-1)
 
 
-def generate_ephemeral_key():
+def generate_counter(start_time: int, current_time: int, interval_in_sec: int) -> int:
+    return floor((current_time - start_time) / interval_in_sec)
+
+def generate_hmac(key: str, start_time: int, current_time: int, counter: int, mode: str) -> str:
+    return hmac.new(bytes(key, 'utf-8'), msg=counter.to_bytes(8, 'big'), digestmod=hashlib.sha1)
+
+def truncate(hashByteArray: bytearray) -> bytearray:
+    offset = hashByteArray[-1] & 0xF
+    print(f"offset: {offset}")
+    code = (
+        (hashByteArray[offset] & 0x7F) << 24
+        | (hashByteArray[offset + 1] & 0xFF) << 16
+        | (hashByteArray[offset + 2] & 0xFF) << 8
+        | (hashByteArray[offset + 3] & 0xFF)
+    )
+    return code
+
+def compute_totp(code: bytearray, digits: int) -> str:
+    str_code = str(10_000_000_000_000_000_000_000_000 + (code % 10**digits))
+    return str_code[-digits :]
+
+def generate_ephemeral_key(verbose: bool, digits: int):
+    """
+    """
     try:
         # Get master key
-        with open(master_key_file, "r") as f:
-            encrypted_key = f.read()
-            password = input("Enter password:")
-            fernet = Fernet(bytes(password, "utf-8"))
-            master_key = fernet.decrypt(bytes(encrypted_key, "utf-8")).decode()
-            print(f"Hex secret: {master_key}")
-#        print(f"Digits: {digits}")
+        with open(master_key_file, "r") as encrypted_file:
+            decrypted_key = decrypt(encrypted_file.read())
+            if not decrypted_key:
+                logging.error("Could not decrypt master key")
 
         # Step 1: Generate an HMAC-SHA-256 value
-        t0 = 0
-        X = 30
-        print(f"Step size (seconds): {X}")
-        now = datetime.now(timezone.utc).replace(microsecond=0)
+        time_step = 30
         start_time = datetime.fromtimestamp(0)
-        print(f"Start time: {start_time} ({int(start_time.timestamp())})")
-        print(f"Current time: {now} ({int(now.timestamp())})")
-#        t = floor(( int(now.timestamp()) - int(start_time.timestamp())) / X) # use floor() ? 
-        t = floor( (1111111109 / 30)) # use floor() ? 
-        print(f"Counter: {hex(t).upper()} ({t})")
-#        hash = hmac.new(bytes(master_key, "utf-8"), msg=bytes(str(t), "utf-8"), digestmod=hashlib.sha256)
-        hash = hmac.new(bytes("12345678901234567890", "utf-8"), msg=t.to_bytes(8, 'big'), digestmod=hashlib.sha1)
+        current_time = datetime.now(timezone.utc).replace(microsecond=0)
+        # counter = floor(( int(now.timestamp()) - int(start_time.timestamp())) / interval_in_sec)  
+        counter = generate_counter(int(start_time.timestamp()), int(current_time.timestamp()), time_step)
 
-        print(hash.hexdigest())
-        hmac_hash = bytearray(hash.digest())
+        hash = hmac.new(bytes(decrypted_key, 'utf-8'), msg=counter.to_bytes(8, 'big'), digestmod=hashlib.sha1)
 
-        offset = hmac_hash[-1] & 0xF
-
-        code = (
-            (hmac_hash[offset] & 0x7F) << 24
-            | (hmac_hash[offset + 1] & 0xFF) << 16
-            | (hmac_hash[offset + 2] & 0xFF) << 8
-            | (hmac_hash[offset + 3] & 0xFF)
-        )
-        print(code)
-        #str_code = str(code % 10**6))
-        #print(str_code)
-        #print(str_code[-6 :])
+        # master key
+        # start time
+        # current time
+        if verbose:
+            print(f"Hex secret: {decrypted_key}")
+            print(f"Digits: {digits}")
+            print(f"Step size (seconds): {interval_in_sec}")
+            print(f"Start time: {start_time} ({int(start_time.timestamp())})")
+            print(f"Current time: {now} ({int(current_time.timestamp())})")
+            print(f"Counter: {hex(counter).upper()} ({counter})")
 
         # Step 2: Generate a 4-byte string (Dynamic Truncation)
 
-        # Step 3: Compute an HOTP value
-        # snum = string to num
-        # return snum % 10%(len(str(snum)))
+        hashByteArray = bytearray(hash.digest())
+        print(f"should be 20-byte string {len(hashByteArray)}")
+
+        code = truncate(hashByteArray)
+        # Step 3: Compute a HOTP value 
+        
+        totp = compute_totp(code, digits)
+        
+        return totp
 
     except IOError as error:
         print(error)
@@ -119,14 +151,30 @@ def generate():
         help="generate a time-based ephemeral password from the master key",
     )
 
+    group.add_argument(
+        "--digits",
+        "-d",
+        type=int,
+        choices=range(6, 10),
+        default=6,
+        help="the length of the TOTP password",
+    )
+
+    group.add_argument(
+        "--verbose",
+        "-v",
+        action="store_true",
+        help="display steps",
+    )
+
     args = parser.parse_args()
 
     logging.basicConfig(filename="otp.log", level=logging.ERROR)
 
     if args.master_key is not None:
-        generate_master_key(args.master_key)
+        generate_master_key(args.verbose, args.master_key)
     elif args.key is True:
         # TODO: check there is a key file with correct permission etc
-        generate_ephemeral_key()
+        generate_ephemeral_key(args.verbose, args.digits)
     else:
         parser.print_help(sys.stderr)
---
+++ tests/tests_vector_totp.py
@@ -0,0 +1,83 @@
+from datetime import datetime
+import hashlib
+from otp import generate_counter, truncate, compute_totp
+import hmac
+secret = "12345678901234567890"
+secretInHex = secret.encode('utf-8').hex()
+start_time = datetime.fromtimestamp(0)
+time_step = 30
+digits = 8
+testTime = [59, 1111111109, 1111111111, 1234567890, 2000000000, 20000000000]
+testMode = [hashlib.sha1, hashlib.sha256, hashlib.sha512]
+
+def test_totp_0():
+
+    counter = generate_counter(int(start_time.timestamp()), int(testTime[0]), time_step)
+    counterToBytes = counter.to_bytes(8, 'big')
+    
+    # SHA1
+    hash = hmac.new(bytes(secret, 'utf-8'), msg=counterToBytes, digestmod=hashlib.sha1)
+    hashByteArray = bytearray(hash.digest())
+    code = truncate(hashByteArray)
+    totp = compute_totp(code, digits)
+    assert totp == '94287082'
+
+def test_totp_3():
+
+    counter = generate_counter(int(start_time.timestamp()), int(testTime[1]), time_step)
+    counterToBytes = counter.to_bytes(8, 'big')
+
+    #SHA1
+    hash = hmac.new(bytes(secret, 'utf-8'), msg=counterToBytes, digestmod=hashlib.sha1)
+    hashByteArray = bytearray(hash.digest())
+    code = truncate(hashByteArray)
+    totp = compute_totp(code, digits)
+    assert totp == '07081804'
+
+def test_totp_6():
+
+    counter = generate_counter(int(start_time.timestamp()), int(testTime[2]), time_step)
+    counterToBytes = counter.to_bytes(8, 'big')
+
+    #SHA1
+    hash = hmac.new(bytes(secret, 'utf-8'), msg=counterToBytes, digestmod=hashlib.sha1)
+    hashByteArray = bytearray(hash.digest())
+    code = truncate(hashByteArray)
+    totp = compute_totp(code, digits)
+    assert totp == '14050471'
+
+def test_totp_9():
+
+    counter = generate_counter(int(start_time.timestamp()), int(testTime[3]), time_step)
+    counterToBytes = counter.to_bytes(8, 'big')
+
+    #SHA1
+    hash = hmac.new(bytes(secret, 'utf-8'), msg=counterToBytes, digestmod=hashlib.sha1)
+    hashByteArray = bytearray(hash.digest())
+    code = truncate(hashByteArray)
+    totp = compute_totp(code, digits)
+    assert totp == '89005924'
+
+def test_totp_12():
+
+    counter = generate_counter(int(start_time.timestamp()), int(testTime[4]), time_step)
+    counterToBytes = counter.to_bytes(8, 'big')
+
+    #SHA1
+    hash = hmac.new(bytes(secret, 'utf-8'), msg=counterToBytes, digestmod=hashlib.sha1)
+    hashByteArray = bytearray(hash.digest())
+    code = truncate(hashByteArray)
+    totp = compute_totp(code, digits)
+    assert totp == '69279037'
+
+def test_totp_15():
+
+    counter = generate_counter(int(start_time.timestamp()), int(testTime[5]), time_step)
+    counterToBytes = counter.to_bytes(8, 'big')
+
+    #SHA1
+    hash = hmac.new(bytes(secret, 'utf-8'), msg=counterToBytes, digestmod=hashlib.sha1)
+    hashByteArray = bytearray(hash.digest())
+    code = truncate(hashByteArray)
+    totp = compute_totp(code, digits)
+    assert totp == '65353130'
